0d168dbe2c0b92f25f1f91f0ae0236152389c4a8
Nunezjoaquin98 <nunezjoaquin.98@gmail.com>
2020-09-16 20:36:52 -0300

    TP

---
+++ Entidades/Calculadora.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+
+        /// <summary>
+        /// Valida que el operador sea (+ - * /) 
+        /// </summary>
+        /// <param name="operador">Operador a validar</param>
+        /// <returns>Devuelve el operador. si no es valido devuelve + </returns>
+        private static string ValidarOperador(string operador)
+        {
+
+            if (operador == "+" || operador == "-" || operador == "/" || operador == "*")
+            {
+                return operador;
+            }
+            else
+            {
+                return "+";
+            }
+        }
+
+
+        /// <summary>
+        /// valida y realiza la operación pedida entre dos numeros
+        /// </summary>
+        /// <param name="num1"> Primer numero de la operacion </param>
+        /// <param name="num2"> Segundo numero de la operacion </param>
+        /// <param name="operador"> Operacion a realizar </param>
+        /// <returns> Resultado de la operacion </returns>
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            operador = ValidarOperador(operador);  //Se valida que el operador sea correcto
+            double aux = double.MinValue;
+            switch (operador)
+            {
+                case "+":                       //SUMA
+                    aux = num1 + num2;
+                    break;
+
+                case "-":                       // RESTA
+                    aux = num1 - num2;
+                    break;
+
+                case "*":                       //MULTIPLICACION         
+                    aux = num1 * num2;
+                    break;
+
+                case "/":                       //DIVISION
+                    aux = num1 / num2;
+                    break;
+            }
+
+            return aux;
+        }
+
+
+
+
+    }
+}
---
+++ MiCalculadora/MiCalculadora.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void MiCalculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //OPERACIONES
+
+        /// <summary>
+        /// Realiza el calculo de dos numeros
+        /// </summary>
+        /// <param name="numero1">numero 1</param>
+        /// <param name="numero2">numero 2</param>
+        /// <param name="operador">operador</param>
+        /// <returns>Resultado del calculon</returns>
+        private double Operar(string numero1, string numero2, string operador)
+        {
+            return Calculadora.Operar(new Numero(numero1), new Numero(numero2), operador);
+        }
+
+        /// <summary>
+        /// Realiza la operacion entre el txtNumero1 y el txtNumero2 con el operador de cmbOperador
+        /// Mostrando el resultado en lblResultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text).ToString();
+        }
+
+
+        /// <summary>
+        /// Convierte el resultado en Binario de ser posible.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text =  Numero.DecimalBinario(this.lblResultado.Text);
+        }
+
+        /// <summary>
+        /// Convierte el resultado en Decimal de ser posible.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = Numero.BinarioDecimal(lblResultado.Text);
+        }
+
+
+        //OTROS METODOS
+
+
+        /// <summary>
+        /// Cierra el form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Llama al metodo Limpiar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+        /// <summary>
+        /// Deja todos los campos en limpio
+        /// </summary>
+        private void Limpiar()
+        {
+            this.txtNumero1.Text = string.Empty;
+            this.txtNumero2.Text = string.Empty;
+            this.cmbOperador.Text = string.Empty;
+            this.lblResultado.Text = "0";
+
+        }
+    }
+}
